7c55b101f6d348c6b33fe15f7728db894c44efa9
Serhii Pirohov <serhii_pirohov@epam.com>
2020-10-28 13:56:20 +0200

    Update test_lamda.py

--- tests/test_lamda.py
+++ tests/test_lamda.py
@@ -6,9 +6,5 @@ def test_lambda():
         "notification_type": "ui",
         "test_id": "d3caeeb7-32bd-4b4b-824d-702708a7116c",
         "report_id": 176,
-        "smtp_host": "smtp.gmail.com",
-        "smtp_port": 587,
-        "smtp_user": "serhii.pirohov@gmail.com",
-        "smtp_password": "Semen4ik20#12345"
     }
     lambda_handler(event, {})
